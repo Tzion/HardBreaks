6d13336edba0d3275f658dd43c3737ff3af18a23
zions <tzion12345@gmail.com>
2025-09-06 13:23:19 +0300

    crack demo

--- demo.js
+++ demo.js
@@ -1,3 +1,4 @@
+// RUM: canvas-sketch 
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random')
 const { Point } = require('./geometry');
@@ -9,6 +10,9 @@ const settings = {
 };
 
 const sketch = ({ context, width, height }) => {
+    /** @type {CanvasRenderingContext2D} */
+    const ctxs = context;
+    ctxs.beginPath
   const crack = new Crack(new Point(50 + random.range(width - 50), 0), new Point(50 + random.range(width - 50), height));
   console.log(crack);
   return ({ context, width, height }) => {
@@ -27,7 +31,7 @@ class CrackPath {
 
   draw(context) {
     this.singleLine(context);
-    this.singleLine(context);
+    // this.singleLine(context);
   }
 
   singleLine(context) {
@@ -35,24 +39,40 @@ class CrackPath {
     context.moveTo(this.start.x, this.start.y);
     const steps = random.rangeFloor(5, 20); // Number of segments to create a noisy line
 
+    // Draw first noisy line to end point
     for (let i = 1; i < steps; i++) {
-      // Interpolate position between start and end
-      const frequency = random.range(20, 50); // Scale factor for noise
-      const amplitude = random.range(20, 50); // How much the line can deviate
+      const frequency = random.range(20, 110);
+      const amplitude = random.range(20, 100);
       const t = i / steps;
       const x = this.start.x + (this.end.x - this.start.x) * t;
       const y = this.start.y + (this.end.y - this.start.y) * t;
 
-      // Add noise offset
       const noiseX = random.noise2D(x, y, frequency, amplitude);
       const noiseY = random.noise2D(y, x, frequency, amplitude);
 
-      // Draw line to this noisy point
       context.lineTo(x + noiseX, y + noiseY);
-      context.stroke();
     }
     context.lineTo(this.end.x, this.end.y);
+
+    // Second line (bottom path) - going back to start
+    // DON'T call beginPath() here - we want to continue the same path
+    for (let i = steps - 1; i >= 0; i--) {
+      const frequency = random.range(20, 520);
+      const amplitude = random.range(20, 50);
+      const t = i / steps;
+      const x = this.start.x + (this.end.x - this.start.x) * t;
+      const y = this.start.y + (this.end.y - this.start.y) * t;
+
+      // Create different noise for the second line
+      const noiseX = random.noise2D(x + 100, y + 100, frequency, amplitude);
+      const noiseY = random.noise2D(y + 100, x + 100, frequency, amplitude);
+
+      context.lineTo(x + noiseX, y + noiseY);
+    }
     context.stroke();
+    context.closePath();
+    context.fillStyle = 'red'
+    context.fill()
 
   }
 }
