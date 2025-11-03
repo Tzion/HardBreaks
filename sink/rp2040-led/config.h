e22f91a99c174d92d828e68d187f831696e7fc1e
zions <tzion12345@gmail.com>
2025-11-03 19:15:28 +0200

    config-h

---
+++ sink/rp2040-led/config.h
@@ -0,0 +1,13 @@
+// Auto-generated from config.js - DO NOT EDIT
+// Generated: 2025-11-03T17:09:59.774Z
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define MATRIX_WIDTH 49
+#define MATRIX_HEIGHT 39
+#define NUM_LEDS (MATRIX_WIDTH * MATRIX_HEIGHT)
+#define FRAME_SIZE (NUM_LEDS * 3)
+#define SERIAL_BAUD 115200
+
+#endif
