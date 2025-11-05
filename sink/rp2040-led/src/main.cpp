017b234092586a50fc4d6362b10bb08eea88a1e8
zions <tzion12345@gmail.com>
2025-11-05 23:22:11 +0200

    test all gpios

--- sink/rp2040-led/src/main.cpp
+++ sink/rp2040-led/src/main.cpp
@@ -11,21 +11,26 @@ uint8_t frameBuffer[FRAME_SIZE];
 uint32_t frameCount = 0;
 
 void diagnoseFrame(uint8_t *frame);
+void blinkGpioOneByOne();
+void setPinsToOutput();
 
 void setup()
 {
+  pinMode(ONBOARD_LED, OUTPUT);
+  setPinsToOutput();
+
   Serial.begin(SERIAL_BAUD);
   while (!Serial && millis() < 7800)
     ;
   if (Serial)
     blink(3000, 10);
-  blink(2000, 1);
   Serial.println("RP2040 ready");
   printf("frame size: %d\n", FRAME_SIZE);
 }
 
 void loop()
 {
+
   if (Serial.available() >= FRAME_SIZE)
   {
     size_t n = Serial.readBytes(frameBuffer, FRAME_SIZE);
@@ -34,6 +39,7 @@ void loop()
 
     frameCount++;
 
+    //digitalWrite(5, HIGH);
     // Blink logic to indicate continous communication
     if (frameCount % 10 == 0)
     {
@@ -43,13 +49,21 @@ void loop()
     {
       digitalWrite(ONBOARD_LED, LOW);
     }
-    printf("%d frames received at %d \n", frameCount, millis());
+    printf("%lu frames received at %lu \n", frameCount, millis());
     // diagnoseFrame(frameBuffer);
   }
+
   if (millis() % HEARTBEAT_MS < HEARTBEAT_MS / 10)
+  {
     digitalWrite(ONBOARD_LED, HIGH);
-  else
+    digitalWrite(5, HIGH);
+  }
+  else {
     digitalWrite(ONBOARD_LED, LOW);
+    digitalWrite(5, LOW);
+  }
+  blinkGpioOneByOne();
+
 }
 
 void diagnoseFrame(uint8_t *frame)
@@ -64,3 +78,21 @@ void diagnoseFrame(uint8_t *frame)
          frame[mid], frame[mid + 1], frame[mid + 2],
          frame[last], frame[last + 1], frame[last + 2]);
 }
+
+void blinkGpioOneByOne(){
+  for (int pin = 0; pin <= 29; ++pin) {
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, HIGH);
+    printf("pin %d is on", pin);
+    delay(120);
+    digitalWrite(pin, LOW);
+  }
+  printf("\n");
+}
+
+void setPinsToOutput() {
+  for (int pin=0; pin <= 29; ++pin){
+    pinMode(pin, OUTPUT);
+  }
+}
+
