54cd516225546ee6931cc632134b40ddb993661b
zions <tzion12345@gmail.com>
2025-11-18 16:34:03 +0200

    improve manual testing2

--- pi/manual_tests.js
+++ pi/manual_tests.js
@@ -12,27 +12,37 @@ async function sendFrame(frameGenerator, ...args) {
 }
 
 
-function markerFrame(index) {
+function markerFrame(index, color = 'green') {
     var marker = new Uint8Array([
-        0, 255, 0,
+        color == 'red' ? 255 : 0,
+        color == 'green' ? 255 : 0,
+        color == 'blue' ? 255 : 0,
     ]);
     const padCount = index;
     var padding = new Uint8Array(padCount * 3);
-    var rgbFrame = new Uint8Array(marker.length + padding.length);
-    rgbFrame.set(padding, 0);
-    rgbFrame.set(marker, padding.length);
-    console.log(rgbFrame)
-    return rgbFrame;
+    var frame = new Uint8Array(marker.length + padding.length);
+    frame.set(padding, 0);
+    frame.set(marker, padding.length);
+    console.log(frame)
+    return frame;
 }
 
 
-function rgbFrame() {
+function rgbFrame(padding = 0) {
     // 3 pixels: [R,G,B] each, flattened into a single array
-    return new Uint8Array([
+    const base = new Uint8Array([
         255, 0, 0,
         0, 255, 0,
         0, 0, 255
     ]);
+
+    const padCount = padding;
+    const paddingBytes = new Uint8Array(padCount * 3);
+    const frame = new Uint8Array(paddingBytes.length + base.length);
+    frame.set(paddingBytes, 0);
+    frame.set(base, paddingBytes.length);
+
+    return frame;
 }
 
 // Map function names to actual functions
@@ -50,5 +60,8 @@ if (!frameGenerator) {
     process.exit(1);
 }
 
-const arg = parseInt(process.argv[3]) || 0;
-sendFrame(frameGenerator, arg);
+const args = process.argv.slice(3).map(arg => {
+    const num = parseInt(arg);
+    return isNaN(num) ? arg : num;
+});
+sendFrame(frameGenerator, ...args);
