67ac8cdac240aadebfbf175907280d6077774d40
zions <tzion12345@gmail.com>
2025-11-20 02:19:22 +0200

    adjust heart size parameters

--- pi/art/hardbreaks.js
+++ pi/art/hardbreaks.js
@@ -6,8 +6,8 @@ import canvasSketch from 'canvas-sketch';
 function drawHeart(context, cx, cy, size) {
     context.beginPath();
     for (let t = 0; t < Math.PI * 2; t += 0.08) {
-        const xt = 16 * Math.pow(Math.sin(t), 3);
-        const yt = -(13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t));
+        const xt = 19 * Math.pow(Math.sin(t), 3);
+        const yt = -(13 * Math.cos(t) - 5 * Math.cos(2 * t) - 3 * Math.cos(3 * t) - 1 * Math.cos(4 * t));
         const x = cx + (xt * size) / 16;
         const y = cy + (yt * size) / 16;
         if (t === 0) context.moveTo(x, y); else context.lineTo(x, y);
